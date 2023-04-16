d0a1152c2d81058804b80d612ac0f637a7a5b831
nandanpytech <bilaginandan333@gmail.com>
2023-04-16 18:30:50 +0530

    push

--- index.js
+++ index.js
@@ -13,24 +13,6 @@ if(navClose){
     })
 }
 
-const skillsContent=document.getElementsByClassName("skills_content")
-const skillsHeader=document.querySelectorAll(".skills_header")
-
-function toggleclass(){
-    let itemclass=this.parentNode.className
-
-    for(let i=0;i<skillsContent.length;i++){
-        skillsContent[i].className="skills_content skills_close"
-    }
-
-    if(itemclass==='skills_content skills_close'){
-        this.parentNode.className="skills_content skills_open"
-    }
-}
-
-skillsHeader.forEach(element => {
-    element.addEventListener("click",toggleclass)
-});
 
 
 
